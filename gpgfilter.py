290de6a7e1bbf90c790698ddb0adcbe5acabf715
Matthias <mail@matthiasbock.net>
2015-03-20 01:10:10 +0100

    header filtering works

--- gpgfilter.py
+++ gpgfilter.py
@@ -6,15 +6,30 @@ import gnupg
 # message is piped to this script from stdin 
 lines = sys.stdin.readlines()
 
+log = open('/var/log/exim4/mainlog','a')
+
+# only filter emails from certain senders
+mail = ''.join(lines)
+if mail.find('redmine@interoberlin.de') == -1:
+	print mail.strip()
+	log.write('gpgfilter: not encrypting\n')
+	exit(0)
+
+log.write('gpgfilter: message encrypted\n')
+
 # strip unencrypted Redmine headers
-dont_show = ['X-Mailer: Redmine\n']
+dont_show = ['X-Mailer: Redmine\n', 'X-Redmine-', 'Subject:']
 i = 0
+# for all lines:
 while (i < len(lines)):
-    try:
-        dont_show.index( lines[i] )
-        lines.remove( lines[i] )
-        k -= 1
-    except:
+    removed = False
+    for j in range(len(dont_show)):
+        if lines[i].find(dont_show[j]) > -1:
+            log.write('gpgfilter: Removed line "'+lines[i]+'"\n')
+            lines.remove( lines[i] )
+            removed = True
+            break
+    if not removed:
         i += 1
 
 # separate headers from content
@@ -32,7 +47,7 @@ content = ''.join(lines[k+1:])
 gpg = gnupg.GPG()
 
 # TODO: derive recipients from To,CC,BCC and Received:for headers
-recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de']
+recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com']
 
 content = str(gpg.encrypt(content, recipients, always_trust=True))
 
@@ -41,3 +56,5 @@ message = headers+'\n'+content
 
 # output manipulated message
 print message
+
+log.close()
