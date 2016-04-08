bf4b51946c7379eec8b3949359a0bdadce88f64b
Matthias <mail@matthiasbock.net>
2016-04-08 15:07:55 +0200

    New feature: Encrypt GitLab-Emails too

--- gpgfilter.py
+++ gpgfilter.py
@@ -21,74 +21,144 @@ if debug:
 
 # only filter emails from certain senders
 mail = ''.join(lines)
-if mail.find('redmine@interoberlin.de') == -1:
+
+if mail.find('redmine@interoberlin.de') != -1 :
+	# strip unencrypted Redmine headers
+	dont_show = ['X-Mailer: ', 'X-Redmine-', 'Subject: ','List-Id: ']
+	i = 0
+	# for all lines:
+	while (i < len(lines)):
+		removed = False
+
+		# for all field not to show:
+		for j in range(len(dont_show)):
+			# does current line contain the not to be shown field?
+			if lines[i].find(dont_show[j]) > -1:
+				# found: remove
+				if debug:
+					log.write('gpgfilter: removed line "'+lines[i].replace('\n','')+'"\n')
+				lines.remove( lines[i] )
+				removed = True
+				
+				# also cut following lines, if they belong to current line
+				while (len(lines[i]) > 0 and lines[i][0] == ' '):
+					if debug:
+						log.write('gpgfilter: also removed line "'+lines[i].replace('\n','')+'"\n')
+					lines.remove( lines[i] )
+					
+				break
+
+		# if current line was removed, i already points to next line
+		if not removed:
+			i += 1
+
+	# separate headers from content
+	for k in range(len(lines)):
+		# the first empty line marks the end of header
+		# and beginning of text sections
+		if lines[k] == '' or lines[k] == '\n':
+			break
+
+	# array -> text
+	headers = ''.join(lines[:k])
+	content = ''.join(lines[k+1:])
+
+	headers += 'Subject: [Redmine]\n'
+
+	# encrypt content
+	gpg = gnupg.GPG(gnupghome='/var/spool/exim4/.gnupg/')
+
+	# TODO: derive recipients from To,CC,BCC and Received:for headers
+	recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org']
+	# recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org','julian.weissgerber@geulengracht.de']
+
+	if debug:
+		log.write('Available keys:\n'+pprint.pformat(gpg.list_keys())+'\n')
+		log.write(content+'\n')
+
+	content = str(gpg.encrypt(content, recipients, always_trust=True))
+
+	if debug:
+		log.write(content)
+	else:
+		log.write('gpgfilter: message encrypted\n')
+
+	# re-assemble headers and content
+	message = headers+'\n'+content
+
+	# output manipulated message
+	print message
+	
+elif mail.find('gitlab@interoberlin.de') != -1 :
+# strip unencrypted Gitlab headers
+	dont_show = ['X-Mailer: ', 'Subject: ','List-Id: ']
+	i = 0
+	# for all lines:
+	while (i < len(lines)):
+		removed = False
+
+		# for all field not to show:
+		for j in range(len(dont_show)):
+			# does current line contain the not to be shown field?
+			if lines[i].find(dont_show[j]) > -1:
+				# found: remove
+				if debug:
+					log.write('gpgfilter: removed line "'+lines[i].replace('\n','')+'"\n')
+				lines.remove( lines[i] )
+				removed = True
+				
+				# also cut following lines, if they belong to current line
+				while (len(lines[i]) > 0 and lines[i][0] == ' '):
+					if debug:
+						log.write('gpgfilter: also removed line "'+lines[i].replace('\n','')+'"\n')
+					lines.remove( lines[i] )
+					
+				break
+
+		# if current line was removed, i already points to next line
+		if not removed:
+			i += 1
+
+	# separate headers from content
+	for k in range(len(lines)):
+		# the first empty line marks the end of header
+		# and beginning of text sections
+		if lines[k] == '' or lines[k] == '\n':
+			break
+
+	# array -> text
+	headers = ''.join(lines[:k])
+	content = ''.join(lines[k+1:])
+
+	headers += 'Subject: [Gitlab]\n'
+
+	# encrypt content
+	gpg = gnupg.GPG(gnupghome='/var/spool/exim4/.gnupg/')
+
+	# TODO: derive recipients from To,CC,BCC and Received:for headers
+	recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org']
+	# recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org','julian.weissgerber@geulengracht.de']
+	
+	if debug:
+		log.write('Available keys:\n'+pprint.pformat(gpg.list_keys())+'\n')
+		log.write(content+'\n')
+
+	content = str(gpg.encrypt(content, recipients, always_trust=True))
+
+	if debug:
+		log.write(content)
+	else:
+		log.write('gpgfilter: message encrypted\n')
+
+	# re-assemble headers and content
+	message = headers+'\n'+content
+
+	# output manipulated message
+	print message
+
+else:
     print mail.strip()
     log.write('gpgfilter: not encrypting\n')
     exit(0)
-
-# strip unencrypted Redmine headers
-dont_show = ['X-Mailer: ', 'X-Redmine-', 'Subject: ','List-Id: ']
-i = 0
-# for all lines:
-while (i < len(lines)):
-    removed = False
-
-    # for all field not to show:
-    for j in range(len(dont_show)):
-        # does current line contain the not to be shown field?
-        if lines[i].find(dont_show[j]) > -1:
-            # found: remove
-            if debug:
-                log.write('gpgfilter: removed line "'+lines[i].replace('\n','')+'"\n')
-            lines.remove( lines[i] )
-            removed = True
-            
-            # also cut following lines, if they belong to current line
-            while (len(lines[i]) > 0 and lines[i][0] == ' '):
-                if debug:
-                    log.write('gpgfilter: also removed line "'+lines[i].replace('\n','')+'"\n')
-                lines.remove( lines[i] )
-                
-            break
-
-    # if current line was removed, i already points to next line
-    if not removed:
-        i += 1
-
-# separate headers from content
-for k in range(len(lines)):
-    # the first empty line marks the end of header
-    # and beginning of text sections
-    if lines[k] == '' or lines[k] == '\n':
-        break
-
-# array -> text
-headers = ''.join(lines[:k])
-content = ''.join(lines[k+1:])
-
-headers += 'Subject: [Redmine]\n'
-
-# encrypt content
-gpg = gnupg.GPG(gnupghome='/var/spool/exim4/.gnupg/')
-
-# TODO: derive recipients from To,CC,BCC and Received:for headers
-recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org']
-
-if debug:
-    log.write('Available keys:\n'+pprint.pformat(gpg.list_keys())+'\n')
-    log.write(content+'\n')
-
-content = str(gpg.encrypt(content, recipients, always_trust=True))
-
-if debug:
-    log.write(content)
-else:
-    log.write('gpgfilter: message encrypted\n')
-
-# re-assemble headers and content
-message = headers+'\n'+content
-
-# output manipulated message
-print message
 
 log.close()
