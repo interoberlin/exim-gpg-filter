301de76567e242953531d33bbcadc02b6ba0415d
Matthias <mail@matthiasbock.net>
2016-05-08 16:32:11 +0200

    GMail / Mailvelope requires Content-Transfer-Encoding to be 8-bit

---
+++ gpg2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from subprocess import Popen,PIPE
+from shlex import split
+
+executable = "/usr/bin/gpg2"
+
+def encrypt(plaintext, recipients):
+    
+    cmd  = executable+" --no-verbose --quiet --trust-model always "
+    r    = ['-r '+email for email in recipients]
+    cmd += " ".join(r)
+    cmd += " --yes --charset utf-8 --armor --output - -e"
+    
+    # prevent mail loss upon exception
+    try:
+        pgp_message = Popen(split(cmd), stdin=PIPE, stdout=PIPE).communicate(input=plaintext)[0]
+    except:
+        return plaintext
+    
+    lines = pgp_message.split('\n')
+    lines.insert(1, 'Charset: UTF-8')
+    cryptotext = '\n'.join(lines)
+    
+    return cryptotext
+    
--- gpgfilter.py
+++ gpgfilter.py
@@ -6,7 +6,7 @@
 #
 
 import sys
-import gnupg
+from gpg2 import encrypt
 import pprint
 from subprocess import Popen,PIPE
 
@@ -24,7 +24,7 @@ mail = ''.join(lines)
 
 if mail.find('redmine@interoberlin.de') != -1 :
 	# strip unencrypted Redmine headers
-	dont_show = ['X-Mailer: ', 'X-Redmine-', 'Subject: ','List-Id: ']
+	dont_show = ['X-Mailer: ', 'X-Redmine-', 'Subject: ','List-Id: ', 'Content-Transfer-Encoding: ']
 	i = 0
 	# for all lines:
 	while (i < len(lines)):
@@ -64,24 +64,27 @@ if mail.find('redmine@interoberlin.de') != -1 :
 	content = ''.join(lines[k+1:])
 
 	headers += 'Subject: [Redmine]\n'
+	headers += 'Content-Transfer-Encoding: 8bit\n'
 
 	# encrypt content
-	gpg = gnupg.GPG(gnupghome='/var/spool/exim4/.gnupg/')
+	#gpg = gnupg.GPG(gnupghome='/var/spool/exim4/.gnupg/', gpgbinary="/usr/bin/gpg2")
+	#gpg.encoding = 'utf-8'
 
 	# TODO: derive recipients from To,CC,BCC and Received:for headers
-	recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org']
+	recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org','longpham@hotmail.de','issac.zyborg@gmail.com']
 	# recipients = ['mail@matthiasbock.net','florian.schwanz@interoberlin.de','t.kuban@googlemail.com','lukas@autistici.org','julian.weissgerber@geulengracht.de']
 
-	if debug:
-		log.write('Available keys:\n'+pprint.pformat(gpg.list_keys())+'\n')
-		log.write(content+'\n')
+	#if debug:
+	#	log.write('Available keys:\n'+pprint.pformat(gpg.list_keys())+'\n')
+	#	log.write(content+'\n')
 
-	content = str(gpg.encrypt(content, recipients, always_trust=True))
+	#content = str(gpg.encrypt(content, recipients, always_trust=True))
+	content = encrypt(content, recipients)
 
 	if debug:
 		log.write(content)
 	else:
-		log.write('gpgfilter: message encrypted\n')
+		log.write('gpgfilter: Message encrypted for '+','.join(recipients)+'\n')
 
 	# re-assemble headers and content
 	message = headers+'\n'+content
