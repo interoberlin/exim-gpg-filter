6885d7cabe8a8312a4de2388c640698d0d888e16
Matthias <mail@matthiasbock.net>
2016-07-02 19:40:09 +0200

    implemented Redmine mail parsing

--- redmine.py
+++ redmine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from email import *
+#from email import *
 
 #
 # Class for Redmine-specific Email parsing
@@ -9,19 +9,59 @@ class RedmineMail:
     #
     # Instantiate with reference to existing Email instance
     #
-    def __init__(self, email):
-        self.mail = email
-    
+    # @param text: Email as array of lines without \n line ending
+    #
+    def __init__(self, lines):
+        self.lines = lines
+
+    #
+    # Find the line, which separates the Redmine change summary from the attached Redmine ticket body
+    #
+    # @return: Line index of separator; counting starts with 0
+    #
+    def getSeparatorLineIndex(self):
+        # check all lines
+        for i in range(len(self.lines)):
+            # line begins with separator string
+            if self.lines[i].find('--------') == 0:
+                return i
+
+    #
+    # Extract the first line of the Redmine ticket body
+    #
+    def getTicketBodyHeader(self):
+        # ticket body immediately starts after separator line
+        return self.lines[self.getSeparatorLineIndex()+1]
+
     #
     # Parse, to which Tracker the mail content is subject to
-    # returns String
+    #
+    # @return: Tracker as string
     #
     def getTracker(self):
-        return "Test"
+        # the string before the '#'
+        return self.getTicketBodyHeader().split('#')[0].strip()
     
     #
     # Parse, to which Ticket number the mail content is subject to
-    # returns String
     #
-    def getTicket(self):
-        return "123"
+    # @return: Ticket number as string
+    #
+    def getTicketNumber(self):
+        # the number after the '#'
+        return self.getTicketBodyHeader().split('#')[1].split(':')[0]
+
+#
+# Unit-Test
+#
+# Run by executing this file: ./redmine.py
+#
+if __name__ == '__main__':
+    lines = """Ziemlich aktualisiert
+    
+----------------------------------------
+Poison Dart Frog #11293:
+""".split('\n')
+    mail = RedmineMail(lines)
+    print "Tracker: "+mail.getTracker()
+    print "Ticket: #"+mail.getTicketNumber()
